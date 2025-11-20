7633405390a7901639033fb8f7560acce729fdd2
Ricardo Ramirez <rich59943@gmail.com>
2025-11-19 17:25:09 -0800

    changing bcrypt to bcryptjs for RENDER

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,76 +1,65 @@
-const User = require('../models/User'); // Import the User model
-const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
-const jwt = require('jsonwebtoken'); // Import jwt for token generation
-
+const User = require("../models/User"); // Import the User model
+const bcrypt = require("bcryptjs"); // Import bcrypt for password hashing
+const jwt = require("jsonwebtoken"); // Import jwt for token generation
 
 const registerUser = async (req, res) => {
-    
-    
-    
-    
-    try {
-        const { email, password } = req.body;
-        
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
+  try {
+    const { email, password } = req.body;
 
-        // Hash the password before saving it to the database
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create and save the user 
-        const newUser = new User({
-            email,
-            password: hashedPassword
-        });
-        await newUser.save();
-
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-            expiresIn: '1d',
-        });
-
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-        console.error('Registration error:', error);
-        res.status(500).json({ message: "Error registering user" });
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    // Hash the password before saving it to the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create and save the user
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ message: "Error registering user" });
+  }
 };
 
 //Login user if exists
 const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ message: "User does not exist" });
-        }
-
-        // Check if password is correct by comparing plain to the hashed one in db
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: "Invalid password" });
-        }
-
-        // Create a JWT token with user ID as the payload
-        const token = jwt.sign(
-            { id: user._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '1d' }
-        );
-
-        // Return the token and user ID
-        res.status(200).json({ token, userId: user._id });
-    } catch (error) {
-        console.error('Login error backend:', error);
-        res.status(500).json({ message: "Error logging in" });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
     }
+
+    // Check if password is correct by comparing plain to the hashed one in db
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // Create a JWT token with user ID as the payload
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // Return the token and user ID
+    res.status(200).json({ token, userId: user._id });
+  } catch (error) {
+    console.error("Login error backend:", error);
+    res.status(500).json({ message: "Error logging in" });
+  }
 };
 
-
-module.exports = { registerUser, loginUser }; //. export both function 
-
-
-
+module.exports = { registerUser, loginUser }; //. export both function
